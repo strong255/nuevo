b9bcb921b81aa8fce147569666be1e51301d4297
strong255 <113145447+strong255@users.noreply.github.com>
2023-01-07 06:24:47 +0000

    Add files via upload

---
+++ MENUS DESPLEGABLES EXAMPLES/festejo1.js
@@ -0,0 +1,32 @@
+
+
+const typed = new Typed('.typed', {
+strings: ['<i class="mascota">CONMIGO</i>',
+                 '<i class="mascota">CONTIGO</i>',
+                  '<i class="mascota">NOSOTROS</i>',
+                   '<i class="mascota">Y</i>',
+                   '<i class="mascota">CON TODOS</i>'
+],
+
+typeSpeed:85,
+startDelay:1000,
+backSpeed:120,
+
+shuffle:false,
+backDelay:500,
+loop:true,
+loopCount:false,
+showCursor:true,
+cursorChar:'|',
+contentType:'html'
+});
+
+
+
+const title = new Tittle('.title', {
+strings: ['ANGEL BUENO', 'FESTEJO', 'EL', 'ANGEL', 'BUENO'
+],
+
+
+
+}); 
